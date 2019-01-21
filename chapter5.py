33ddbfa47e1050552b3764d6003e2765ee969e2d
raymondtseng <raymondtseng0912@gmail.com>
2019-01-22 00:27:58 +0800

    finish a part of chapter5

--- chapter5.py
+++ chapter5.py
@@ -38,3 +38,73 @@ class C: pass
 obj = C()
 def func(): pass
 print(sorted(set(dir(func)) - set(dir(obj))))
+
+# 从定位参数到仅限关键字参数
+def tag(name, *content, cls=None, **attrs):
+    """生成一个或多个HTML标签"""
+    if cls is not None:
+        attrs['class'] = cls
+    if attrs:
+        attr_str = ''.join(' %s="%s"' % (attr, value) for attr, value in sorted(attrs.items()))
+    else:
+        attr_str = ''
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' % (name, attr_str, c, name) for c in content)
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+print(tag('br'))
+print(tag('p', 'hello'))
+print(tag('p', 'hello', 'world'))
+print(tag('p', 'hello', id=33))
+print(tag('p', 'hello', 'world', cls='sidebar'))
+print(tag(content='testing', name='img'))
+my_tag = {'name': 'img', 'title': 'Sunset Boulevard', 'src': 'sunset.jpg', 'cls': 'framed'}
+print(tag(**my_tag))
+
+# 装饰器可以把一个普通函数与框架的请求处理机制结合起来，如果一个函数需要一个person参数
+# 则可以从请求中获取那个名称对应的参数，那请求框架怎么知道函数需要哪个参数呢？
+# 函数对象有__defaults__属性，是一个元组，里面保存定位参数和关键字参数的默认值
+# 仅限关键字参数的默认值在__kwdefaults__属性中
+# 参数的名称在__code__属性中
+
+# 可以用inspect提取函数的签名，即获取函数参数的信息
+from inspect import signature
+sig = signature(tag)
+print(sig)
+print(str(sig))
+for name, param in sig.parameters.items():
+    print(param.kind, ':', name, param.default)
+
+# inspect._empty表示没有默认值
+# 可以使用bind方法把任意个参数绑定到签名中的形参上
+
+bound_args = sig.bind(**my_tag)
+for name, value in bound_args.arguments.items():
+    print(name, '=', value)
+
+# 如果删除my_tag中其中一个值，则bind()方法会报错，缺失了相关值
+
+# 函数注解
+def clip(text:str, max_len:'int > 0'=80) -> str:
+    """
+    在max_len前面或后面的第一个空格处截断文本
+    :param text:
+    :param max_len:
+    :return:
+    """
+    end = None
+    if len(text) > max_len:
+        space_before = text.rfind(' ', 0, max_len)
+        if space_before >= 0:
+            end = space_before
+        else:
+            space_after = text.rfind(' ', max_len)
+            if space_after >= 0:
+                end = space_after
+    if end is None:
+        end = len(text)
+    return text[:end].rstrip()
+
+# 注解不会做任何处理，只是存储在函数的__annotations__属性中（一个字典）
+# 可以使用signature.return_annotation提取注解
